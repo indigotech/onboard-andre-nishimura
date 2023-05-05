43e785faffdf68b8e51cdedd92eb5c6eef7ad8f3
AndreHGN <andrehidekinishimura@gmail.com>
2023-05-05 14:35:32 -0300

    Apply email and password validations to login form

--- src/components/password-input.tsx
+++ src/components/password-input.tsx
@@ -1,17 +1,26 @@
 import React, { Fragment } from 'react';
+import { FormFieldErrorList } from './form-field-error-list';
 
 interface PasswordInputProps {
   name: string;
   label: string;
   value: string | undefined;
   onValueChange: (value: string) => void;
+  errors: string[];
 }
 
-export const PasswordInput = ({ name, label, value, onValueChange }: PasswordInputProps): React.ReactElement => {
+export const PasswordInput = ({
+  name,
+  label,
+  value,
+  onValueChange,
+  errors,
+}: PasswordInputProps): React.ReactElement => {
   return (
     <Fragment>
       <div>{label}</div>
       <input name={name} type='password' value={value} onChange={(e) => onValueChange(e.target.value)} />
+      <FormFieldErrorList errors={errors} />
     </Fragment>
   );
 };
--- src/components/text-input.tsx
+++ src/components/text-input.tsx
@@ -1,17 +1,20 @@
 import React, { Fragment } from 'react';
+import { FormFieldErrorList } from './form-field-error-list';
 
 interface TextInputProps {
   name: string;
   label: string;
   value: string | undefined;
   onValueChange: (value: string) => void;
+  errors: string[];
 }
 
-export const TextInput = ({ name, label, value, onValueChange }: TextInputProps): React.ReactElement => {
+export const TextInput = ({ name, label, value, onValueChange, errors }: TextInputProps): React.ReactElement => {
   return (
     <Fragment>
       <div>{label}</div>
       <input name={name} type='text' value={value} onChange={(e) => onValueChange(e.target.value)} />
+      <FormFieldErrorList errors={errors} />
     </Fragment>
   );
 };
