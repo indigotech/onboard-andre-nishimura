9e8f0f796cf5d1fe6d17904c04fa76110ddd7f24
AndreHGN <andrehidekinishimura@gmail.com>
2023-05-12 11:29:58 -0300

    Implement pagination behaviour

--- src/components/pagination.tsx
+++ src/components/pagination.tsx
@@ -1,22 +1,78 @@
 import React, { Fragment, useState } from 'react';
 
 export const Pagination = (): React.ReactElement => {
-  const [page, setPage] = useState(1);
-  const maxPage = 4;
-  const selectedPages = selectPagesToShow(page, maxPage);
-  console.log(selectedPages);
+  const [currentPage, setCurrentPage] = useState(0);
+  const maxPage = 7;
+  const neighbours = 2;
+  const selectedPages = selectPagesToShow(currentPage, maxPage, neighbours);
+
+  const pageButtons = selectedPages.map((page) => {
+    if (typeof page == 'string') {
+      return '...';
+    } else {
+      return (
+        <button
+          onClick={() => setCurrentPage(page)}
+          style={currentPage === page ? { backgroundColor: 'lightGray' } : {}}
+        >
+          {page + 1}
+        </button>
+      );
+    }
+  });
+
+  const previousButton = (
+    <button onClick={() => setCurrentPage(currentPage - 1)} disabled={currentPage === 0}>
+      {'<'}
+    </button>
+  );
+
+  const nextButton = (
+    <button onClick={() => setCurrentPage(currentPage + 1)} disabled={currentPage === maxPage}>
+      {'>'}
+    </button>
+  );
 
   return (
     <Fragment>
-      {selectedPages?.map((page) => {
-        return <button>{page + 1}</button>;
-      })}
+      {previousButton}
+      {pageButtons}
+      {nextButton}
     </Fragment>
   );
 };
 
-const selectPagesToShow = (currentPage: number, maxPage: number): number[] | void => {
-  if (maxPage < 5) {
-    return Array.from({ length: maxPage }, (value, index) => index);
+const selectPagesToShow = (currentPage: number, maxPage: number, neighbours: number): (number | string)[] => {
+  const expectedTotalPages = neighbours * 2 + 3;
+
+  if (maxPage + 1 > expectedTotalPages) {
+    const startPage = Math.max(currentPage - neighbours, 1);
+    const endPage = Math.min(currentPage + neighbours, maxPage - 1);
+
+    let selectedPages: (number | string)[] = range(startPage, endPage);
+
+    const pagesCount = selectedPages.length;
+    const extraPagesQuantity = expectedTotalPages - pagesCount - 2;
+
+    const isLeftEllipsed = startPage > 1;
+    const isRightEllipsed = endPage < maxPage - 1;
+
+    if (isLeftEllipsed && !isRightEllipsed) {
+      const extraPages = range(startPage - extraPagesQuantity, startPage - 1);
+      selectedPages = ['...', ...extraPages, ...selectedPages];
+    } else if (!isLeftEllipsed && isRightEllipsed) {
+      const extraPages = range(endPage + 1, endPage + extraPagesQuantity);
+      selectedPages = [...selectedPages, ...extraPages, '...'];
+    } else if (isLeftEllipsed && isRightEllipsed) {
+      selectedPages = ['...', ...selectedPages, '.,.'];
+    }
+
+    return [0, ...selectedPages, maxPage];
   }
+
+  return range(0, maxPage);
+};
+
+const range = (start: number, end: number): number[] => {
+  return Array.from({ length: end - start + 1 }, (_, index) => start + index);
 };
