92aa3f5ff17309c8cffe64bf73ff5a5b814a1cb6
AndreHGN <andrehidekinishimura@gmail.com>
2023-05-18 16:08:43 -0300

    Apply navigation to user details page when clicking on an item of the user list

--- src/components/user-list.tsx
+++ src/components/user-list.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import { User } from '../queries/user-list-query';
 
 interface UserListProps {
@@ -6,12 +7,20 @@ interface UserListProps {
 }
 
 export const UserList = ({ users }: UserListProps): React.ReactElement => {
+  const navigate = useNavigate();
+
   return (
     <ul>
       {users?.map((user: User) => {
         return (
           <li key={user.id}>
-            {user.name} - {user.email}
+            <a
+              onClick={() => {
+                navigate(`/user-details/${user.id}`), { replace: true };
+              }}
+            >
+              {user.name} - {user.email}
+            </a>
           </li>
         );
       })}
