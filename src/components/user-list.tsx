b889c7f63f155f90deaba3eb88aea29a20bf4c4a
AndreHGN <andrehidekinishimura@gmail.com>
2023-05-19 10:11:56 -0300

    CR changes: use proper link instead of anchor tag with button behaviour

--- src/components/user-list.tsx
+++ src/components/user-list.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { User } from '../queries/user-list-query';
 
 interface UserListProps {
@@ -7,20 +7,14 @@ interface UserListProps {
 }
 
 export const UserList = ({ users }: UserListProps): React.ReactElement => {
-  const navigate = useNavigate();
-
   return (
     <ul>
       {users?.map((user: User) => {
         return (
           <li key={user.id}>
-            <a
-              onClick={() => {
-                navigate(`/user-details/${user.id}`), { replace: true };
-              }}
-            >
+            <Link to={`/user-details/${user.id}`}>
               {user.name} - {user.email}
-            </a>
+            </Link>
           </li>
         );
       })}
